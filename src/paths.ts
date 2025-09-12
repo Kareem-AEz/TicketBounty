fb0fe2cfa22a80424a0a729ea4295861ca5f81cb
Kareem-AEz <kemoahmedahmedkemo@gmail.com>
2025-09-12 17:57:15 +0300

    chore: update Prettier configuration and add formatting scripts; enhance header navigation with sign-in and sign-up links

--- src/paths.ts
+++ src/paths.ts
@@ -1,3 +1,7 @@
 export const homePath = () => "/";
 export const ticketsPath = () => "/tickets";
 export const ticketPath = (ticketId: string) => `/tickets/${ticketId}`;
+
+export const signInPath = () => "/sign-in";
+export const signUpPath = () => "/sign-up";
+export const passwordForgotPath = () => "/password-forgot";
